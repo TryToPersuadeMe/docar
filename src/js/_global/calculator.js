1b07adf2b49b754699cd7dfe50e9a1c9c825c3c6
TryToPersuadeMe <61381766+TryToPersuadeMe@users.noreply.github.com>
2021-06-01 15:17:21 +0300

    1

--- src/js/_global/calculator.js
+++ src/js/_global/calculator.js
@@ -6,42 +6,44 @@ const $from_num = document.querySelector(".calculator__from");
 
 const $showResult = document.querySelectorAll(".calculator__customInput");
 
-$dateSlider.forEach((el, index) => {
-  let minNum, maxNum, multiplier, endfix;
+window.addEventListener("load", () => {
+  $dateSlider.forEach((el, index) => {
+    let minNum, maxNum, multiplier, endfix;
 
-  switch (el.offsetParent.getAttribute("id")) {
-    case "cost":
-      (minNum = 5000), (maxNum = 25000), (multiplier = 1000), (endfix = " ₽");
-      break;
+    switch (el.offsetParent.getAttribute("id")) {
+      case "cost":
+        (minNum = 5000), (maxNum = 25000), (multiplier = 1000), (endfix = " ₽");
+        break;
 
-    case "firstPayment":
-      (minNum = 1), (maxNum = 100), (multiplier = 1000), (endfix = " ₽");
-      break;
+      case "firstPayment":
+        (minNum = 1), (maxNum = 100), (multiplier = 1000), (endfix = " ₽");
+        break;
 
-    case "period":
-      (minNum = 6), (maxNum = 32), (multiplier = 1), (endfix = " меc.");
-      break;
-  }
-
-  noUiSlider.create(el, {
-    start: 500,
-    step: 1,
-    connect: [true, false],
-    range: {
-      min: minNum,
-      max: maxNum,
-    },
-  });
-
-  el.noUiSlider.on("update", (values) => {
-    let result = `${Math.round(values[0] * multiplier).toLocaleString()}`;
-    $showResult[index].innerText = result + endfix;
-
-    let note = el.offsetParent.querySelector(".calculator__note");
-    if (note) {
-      let endfix = note.dataset.endfix_note;
-
-      note.innerText = Math.round(values[0]) + endfix;
+      case "period":
+        (minNum = 6), (maxNum = 32), (multiplier = 1), (endfix = " меc.");
+        break;
     }
+
+    noUiSlider.create(el, {
+      start: 500,
+      step: 1,
+      connect: [true, false],
+      range: {
+        min: minNum,
+        max: maxNum,
+      },
+    });
+
+    el.noUiSlider.on("update", (values) => {
+      let result = `${Math.round(values[0] * multiplier).toLocaleString()}`;
+      $showResult[index].innerText = result + endfix;
+
+      let note = el.offsetParent.querySelector(".calculator__note");
+      if (note) {
+        let endfix = note.dataset.endfix_note;
+
+        note.innerText = Math.round(values[0]) + endfix;
+      }
+    });
   });
 });
--- src/js/_global/header.js
+++ src/js/_global/header.js
@@ -1,13 +1,53 @@
-const select = () => {
-  const $wrapper = document.querySelectorAll(".navigation__select");
-  $wrapper.forEach((el) => {
-    el.addEventListener("mouseenter", () => {
-      el.classList.add("open");
-    });
+class responsiveHeader {
+  constructor(props) {
+    this.$navigation = document.querySelector(props.navigation);
+    this.$burgerIcon = document.querySelector(props.burgerIcon);
 
-    el.addEventListener("mouseleave", () => {
-      el.classList.remove("open");
+    this.$header = document.querySelector(".header");
+
+    this.overlay = "overlay";
+    this.$htmlBody = document.querySelector("body");
+
+    this.BurgerClick();
+    this.WindowClick();
+  }
+
+  openState() {
+    this.$navigation.classList.add("active");
+    this.$burgerIcon.classList.add("active");
+    this.$htmlBody.classList.add(this.overlay);
+
+    this.$header.classList.add("opened");
+  }
+
+  closeState() {
+    this.$navigation.classList.remove("active");
+    this.$burgerIcon.classList.remove("active");
+    this.$htmlBody.classList.remove(this.overlay);
+
+    this.$header.classList.remove("opened");
+  }
+
+  BurgerClick() {
+    this.$burgerIcon.addEventListener("click", () => {
+      if (!event.currentTarget.classList.contains("active")) {
+        this.openState();
+      } else {
+        this.closeState();
+      }
     });
-  });
-};
-select();
+  }
+
+  WindowClick() {
+    document.addEventListener("click", (event) => {
+      if (event.target.classList.contains(this.overlay)) {
+        this.closeState();
+      }
+    });
+  }
+}
+
+const headerBurgerMenu = new responsiveHeader({
+  navigation: ".header__navigation",
+  burgerIcon: ".burgerIcon",
+});
